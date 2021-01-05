942336cf1b3566947558b00b9fbad12c9785f6e8
Blessing Ayeni <blessingayeni@Blessings-MacBook-Pro.local>
2021-01-05 20:29:39 +0100

    spew out the data in the recipe struct using spew.SDump

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ayeniblessing101/recipe-book
 
 go 1.15
+
+require github.com/davecgh/go-spew v1.1.1
--- internal/server/server.go
+++ internal/server/server.go
@@ -3,12 +3,25 @@ package server
 
 import (
 	"fmt"
+	"html"
 	"log"
 	"net/http"
+
+	"github.com/ayeniblessing101/recipe-book/internal/models"
+	"github.com/davecgh/go-spew/spew"
 )
 
 func handleHelloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s", "Hello Dima")
+	recipe := &models.Recipe{
+		ID: 23,
+		Name: "Singaporean Noodles",
+		Ingredients: []string{"Rice Noodle", "Red Bell Pepper", "Green Bell Pepper", "Onion", "Garlic"},
+		Directions: []string{"Boil Water", "Soak Noodles in Hot Water", "Fry Vegetables"},
+		Calories: 23,
+	}
+	fmt.Fprintf(w, "%s\n", "Hello Dima")
+	fmt.Fprintf(w, html.EscapeString(spew.Sdump(recipe)))
+
 }
 
 // Server method handles all requests
