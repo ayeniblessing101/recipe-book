5ba45f219575f149dd0a22df543174d30b120056
Blessing Ayeni <blessingayeni@Blessings-MacBook-Pro.local>
2021-01-20 17:17:27 +0100

    remove dead code

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,23 +3,21 @@ package server
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 
 	"github.com/ayeniblessing101/recipe-book/internal/handlers"
-	"github.com/davecgh/go-spew/spew"
 )
 
 func handleHelloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s\n", "Hello Dima")
-	fmt.Fprintf(w, html.EscapeString(spew.Sdump()))
+	
 }
 
 // Server method handles all requests
 func Server(port string) {
 	http.HandleFunc("/", handleHelloWorld)
-	//http.HandleFunc("/categories", handlers.AddCategory)
+	http.HandleFunc("/categories", handlers.AddCategory)
 	http.HandleFunc("/categories", handlers.GetCategories)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
